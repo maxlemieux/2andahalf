5ec8a1a0c3520b01194b8a9e63696f89c6e37455
Max Lemieux <max.lemieux@gmail.com>
2020-05-22 13:45:45 -0700

    get nearby tiles

--- src/components/map.js
+++ src/components/map.js
@@ -1,6 +1,6 @@
 import React from "react";
 import MapRow from "./mapRow";
-
+import getNearbyTiles from "../tileUtil.js";
 import spriteInfo from "../spriteInfo.js";
 const { dungeonSprite, dungeonTiles, floorSprite, floorTiles } = spriteInfo;
 
@@ -20,6 +20,7 @@ function Map(props) {
     backgroundColor: 'gray',
   };
 
+
   const buildMap = (mapWidth, mapHeight) => {
     const worldData = [];
     /* X: j, Y: i */
@@ -31,7 +32,7 @@ function Map(props) {
         const thisX = 32 * j + (window.innerWidth / 4);
 
         /* Get isometric coordinates for this tile */
-        const iso = twoDToIso(thisX, thisY);
+        const { xIso, yIso } = twoDToIso(thisX, thisY);
 
         /* Create the tile with some defaults */
         const tile = {
@@ -43,8 +44,8 @@ function Map(props) {
           y: i,
           xScreen: thisX,
           yScreen: thisY,
-          xIso: iso.x,
-          yIso: iso.y,
+          xIso,
+          yIso,
           z: 1,
         };
         thisRow.push(tile);
@@ -230,6 +231,7 @@ function Map(props) {
     // get size of array to determine potential size of room
     const mapWidth = worldData[0].length;
     const mapHeight = worldData.length;
+
     // check random position and room size, see if it fits
     let roomFound = false;
     while (roomFound === false) {
@@ -240,6 +242,7 @@ function Map(props) {
       // const topLeftX = Math.floor(seedrandom(RANDOM_KEY)() * mapWidth);
       // const topLeftY = Math.floor(seedrandom(RANDOM_KEY)() * mapHeight);
 
+      /* Random room position */
       const topLeftX = Math.floor(Math.random() * mapWidth);
       const topLeftY = Math.floor(Math.random() * mapHeight);
       if (roomWidth + topLeftX < mapWidth && roomHeight + topLeftY < mapHeight && roomFound === false) {
@@ -251,21 +254,48 @@ function Map(props) {
           /* For each tile in that row of the new room */
           for (let j=0; j<roomWidth; j++) {
             const x = topLeftX + j;
+
+            /* Type will be either 'empty', 'wall' or 'ground' */
             const existingTileType = worldData[y][x].type;
 
+            const nearbyTiles = getNearbyTiles(x, y, worldData);
+    
             /* Check the existing tile on the map to see what is there and what to do */
             if (existingTileType === 'wall') {
-              /* This is an old wall from another room, make it into a floor to create a big room.
-                 First, Check if it needs to end up as an outside wall:
-                 If the new wall and the old wall are the same, just keep it. */
               if (worldData[y][x].wallType === 'wall_n' && i === 0 && (j !== 0 && j !== roomWidth - 1)) {
-                /* If we are on the north side of the room  and the old wall is a north wall*/
+                /* If we are on the north side of the new room  and the old wall is a north wall*/
                 console.log(`Found an existing north wall at x: ${x}, y: ${y}`)
+
+              } else if (worldData[y][x].wallType === 'wall_n' && 
+                        (i !== 0 && i !== roomHeight - 1) &&
+                        (j !== 0 && j !== roomWidth - 1)) {
+                /* If the old wall is a north wall
+                   and we are not on the north side of the new room or the south side of the new room
+                   and we are in the floor of the new room,
+                   replace it with the appropriate outside corner */
+                if (nearbyTiles.n.type === 'wall' &&
+                    nearbyTiles.n.wallType === 'wall_w') {
+                    /* outside sw corner */
+                    worldData[y][x].backgroundImage = dungeonSprite;
+                    worldData[y][x].type = 'wall';
+                    worldData[y][x].wallType = 'corner_sw_outer';
+                    worldData[y][x].spriteOffset = dungeonTiles.swo;
+                    worldData[y][x].z = 1;          
+                } else if (nearbyTiles.n.type === 'wall' && 
+                           nearbyTiles.n.wallType === 'wall_e') {
+                    /* outside se corner */
+                    worldData[y][x].backgroundImage = dungeonSprite;
+                    worldData[y][x].type = 'wall';
+                    worldData[y][x].wallType = 'corner_se_outer';
+                    worldData[y][x].spriteOffset = dungeonTiles.seo;
+                    worldData[y][x].z = 1;
+                }
+
               } else if (worldData[y][x].wallType === 'wall_s' && i === (roomHeight - 1)) {
-                /* If we are on the south side of the room  */
+                /* If we are on the south side of the new room  */
                 console.log(`Found an existing south wall at x: ${x}, y: ${y}`)
               } else if (worldData[y][x].wallType === 'wall_w' && j === 0) {
-                /* If we are on the west side of the room  */
+                /* If we are on the west side of the new room  */
                 console.log(`Found an existing west wall at x: ${x}, y: ${y}`)
               } else if (worldData[y][x].wallType === 'wall_e' && j === (roomWidth - 1)) {
                 /* If we are on the east side of the room  */
@@ -287,11 +317,13 @@ function Map(props) {
               } else if (worldData[y][x].wallType === 'corner_nw_inner' && i === 0 && j !== 0 && j !== (roomWidth - 1)) {
                 /* If we are on the northwest corner of the old room and corners do not match */
                 console.log(`Found old inner northwest corner at x: ${x}, y: ${y} - replacing with north wall`)
+
                 worldData[y][x].wallType = 'wall_n';
-                worldData[y][x].spriteOffset = dungeonTiles.n[Math.floor(Math.random() * dungeonTiles.n.length)];      
+                worldData[y][x].spriteOffset = dungeonTiles.n[Math.floor(Math.random() * dungeonTiles.n.length)];  
               } else if (worldData[y][x].wallType === 'corner_ne_inner' && i === 0 && j !== 0 && j !== (roomWidth - 1)) {
                 /* If we are on the northeast corner of the old room and corners do not match*/
                 console.log(`Found old inner northeast corner at x: ${x}, y: ${y} - replacing with north wall`)
+
                 worldData[y][x].wallType = 'wall_n';
                 worldData[y][x].spriteOffset = dungeonTiles.n[Math.floor(Math.random() * dungeonTiles.n.length)];
               } else if (worldData[y][x].wallType === 'corner_sw_inner' && i === (roomHeight - 1) && j === 0) {
@@ -321,13 +353,13 @@ function Map(props) {
               worldData[y][x].backgroundImage = dungeonSprite;
               worldData[y][x].type = 'wall';
 
-              if (i===0) {
+              if (i === 0) {
                 /* NORTH WALL */
-                if (j===0) {
+                if (j === 0) {
                   // northwest corner of room
                   worldData[y][x].wallType = 'corner_nw_inner';
                   worldData[y][x].spriteOffset = dungeonTiles.nw;
-                } else if (j===(roomWidth - 1)) {
+                } else if (j === (roomWidth - 1)) {
                   // northeast corner of room
                   worldData[y][x].wallType = 'corner_ne_inner';
                   worldData[y][x].spriteOffset = dungeonTiles.ne;
@@ -394,11 +426,16 @@ function Map(props) {
     const iso = {};
     iso.x = x - y;
     iso.y = (x + y) / 2;
-    return iso;
+    return {
+      xIso: iso.x, 
+      yIso: iso.y 
+    };
   };
 
   let worldArray = newRoom(worldData);
   worldArray = newRoom(worldData);
+
+  console.log(getNearbyTiles(0, 0, worldArray))
 
   /* Display map */
   return (
---
+++ src/tileUtil.js
@@ -0,0 +1,40 @@
+const getNearbyTiles = (x, y, worldData) => {
+  const nearbyTiles = {
+    w: null,
+    nw: null,
+    n: null,
+    ne: null,
+    e: null,
+    sw: null,
+    s: null,
+    se: null,
+  };
+  if (x > 0) {
+    nearbyTiles.w = worldData[y][x - 1];
+  }
+  if (x > 0 && y > 0) {
+    nearbyTiles.nw = worldData[y - 1][x - 1];
+  }
+  if (y < worldData.length - 1) {
+    nearbyTiles.s = worldData[y + 1][x];
+  }
+  if (x > 0 && y < worldData.length - 1) {
+    nearbyTiles.sw = worldData[y + 1][x - 1];
+  }
+  if (x < worldData[0].length) {
+    nearbyTiles.e = worldData[y][x + 1];
+  }
+  if (x < worldData[0].length && y < worldData.length - 1) {
+    nearbyTiles.se = worldData[y + 1][x + 1];
+  }
+  if (y > 0) {
+    nearbyTiles.n = worldData[y - 1][x];
+  }
+  if (y > 0 && x < worldData[0].length) {
+    nearbyTiles.ne = worldData[y - 1][x + 1];
+  }
+
+  return nearbyTiles;
+};
+
+module.export = getNearbyTiles;
