c8123e89234eb24a5fa4755f55a8aefc2397e0b0
Max Lemieux <max.lemieux@gmail.com>
2020-05-22 08:45:42 -0700

    fill gaps properly in central walls

--- src/components/map.js
+++ src/components/map.js
@@ -111,7 +111,6 @@ function Map(props) {
       const topLeftY = Math.floor(Math.random() * mapHeight);
       if (roomWidth + topLeftX < mapWidth && roomHeight + topLeftY < mapHeight && roomFound === false) {
         roomFound = true;
-        // console.log(`${topLeftX}, ${topLeftY}`)
 
         /* For each row */
         for (let i=0; i<roomHeight; i++) {
@@ -145,22 +144,31 @@ function Map(props) {
               console.log(`Found old wall at x: ${x}, y: ${y}, facing: ${worldData[y][x].facing}, 
                 roomHeight: ${roomHeight}, roomWidth: ${roomWidth} and i = ${i}`)
 
-                if (worldData[y][x].facing === 's' && i === 0) {
-                /* If we are on the north side of the room and the current tile is facing south */
+              /* Need to also check for inside corners that need to remain */
+              if (worldData[y][x].wallType === 'wall_n' && i === 0) {
+                /* If we are on the north side of the room */
                 console.log(`Found an existing north wall at x: ${x}, y: ${y}`)
-                break;
-              } else if (worldData[y][x].facing === 'n' && i === (roomHeight - 1)) {
-                /* If we are on the south side of hte room and the current tile is facing north */
+              } else if (worldData[y][x].wallType === 'wall_s' && i === (roomHeight - 1)) {
+                /* If we are on the south side of the room  */
                 console.log(`Found an existing south wall at x: ${x}, y: ${y}`)
-                break;
-              } else if (worldData[y][x].facing === 'e' && j === 0) {
-                /* If we are on the west side of the room and the current tile is facing east */
+              } else if (worldData[y][x].wallType === 'wall_w' && j === 0) {
+                /* If we are on the west side of the room  */
                 console.log(`Found an existing west wall at x: ${x}, y: ${y}`)
-                break;
-              } else if (worldData[y][x].facing === 'w' && j === (roomWidth - 1)) {
-                /* If we are on the east side of the room and the current tile is facing west */
+              } else if (worldData[y][x].wallType === 'wall_e' && j === (roomWidth - 1)) {
+                /* If we are on the east side of the room  */
                 console.log(`Found an existing east wall at x: ${x}, y: ${y}`)
-                break;
+              } else if (worldData[y][x].wallType === 'corner_nw_inner' && i === 0 && j === 0) {
+                /* If we are on the northwest corner of the room */
+                console.log(`Found an existing inner northwest corner at x: ${x}, y: ${y}`)
+              } else if (worldData[y][x].wallType === 'corner_ne_inner' && i === 0 && j === (roomWidth - 1)) {
+                /* If we are on the northeast corner of the room */
+                console.log(`Found an existing inner northeast corner at x: ${x}, y: ${y}`)
+              } else if (worldData[y][x].wallType === 'corner_sw_inner' && i === (roomHeight - 1) && j === 0) {
+                /* If we are on the southwest corner of the room */
+                console.log(`Found an existing inner southwest corner at x: ${x}, y: ${y}`)
+              } else if (worldData[y][x].wallType === 'corner_se_inner' && i === (roomHeight - 1) && j === (roomWidth - 1)) {
+                /* If we are on the southeast corner of the room */
+                console.log(`Found an existing inner southeast corner at x: ${x}, y: ${y}`)
               } else {
                 console.log(`j=${j} did not match (roomWidth - 1) = ${roomWidth - 1}`)
                 console.log(`i=${i} did not match (roomHeight - 1) = ${roomHeight - 1}`)
@@ -190,14 +198,17 @@ function Map(props) {
                 if (j===0) {
                   // northwest corner of room
                   worldData[y][x].facing = 'se';
+                  worldData[y][x].wallType = 'corner_nw_inner';
                   worldData[y][x].spriteOffset = dungeonTiles.nw;
                 } else if (j===(roomWidth - 1)) {
                   // northeast corner of room
                   worldData[y][x].facing = 'sw';
+                  worldData[y][x].wallType = 'corner_ne_inner';
                   worldData[y][x].spriteOffset = dungeonTiles.ne;
                 } else {
                   // north wall center
                   worldData[y][x].facing = 's';
+                  worldData[y][x].wallType = 'wall_n_various';
                   worldData[y][x].spriteOffset = dungeonTiles.n[Math.floor(Math.random() * dungeonTiles.n.length)];
                 }
               } else if (i === (roomHeight - 1)) {
@@ -205,14 +216,17 @@ function Map(props) {
                 if (j===0) {
                   // southwest corner of room
                   worldData[y][x].facing = 'ne';
+                  worldData[y][x].wallType = 'corner_sw_inner';
                   worldData[y][x].spriteOffset = dungeonTiles.sw;
                 } else if (j === (roomWidth - 1)) {
                   // southeast corner of room
                   worldData[y][x].facing = 'nw';
+                  worldData[y][x].wallType = 'corner_se_inner';
                   worldData[y][x].spriteOffset = dungeonTiles.se;         
                 } else {
                   // south wall center
                   worldData[y][x].facing = 'n';
+                  worldData[y][x].wallType = 'wall_s';
                   worldData[y][x].spriteOffset = dungeonTiles.s;
                 }
               } else {
@@ -221,15 +235,18 @@ function Map(props) {
                 if (j===0) {
                   // west wall of room
                   worldData[y][x].facing = 'e';
+                  worldData[y][x].wallType = 'wall_w';
                   worldData[y][x].spriteOffset = dungeonTiles.w;
                 } else if (j===(roomWidth - 1)) {
                   // east wall of room
                   worldData[y][x].facing = 'w';
+                  worldData[y][x].wallType = 'wall_e';
                   worldData[y][x].spriteOffset = dungeonTiles.e;
                 } else {
                   // floor tile in center of room
                   worldData[y][x].backgroundImage = floorSprite;
                   worldData[y][x].facing = false;
+                  worldData[y][x].wallType = undefined;
                   worldData[y][x].spriteOffset = floorTiles.tiles[Math.floor(Math.random() * floorTiles.tiles.length)];
                   worldData[y][x].type = 'ground';
                   worldData[y][x].walkable = true;
@@ -241,57 +258,82 @@ function Map(props) {
         }; // check row to see what to do
 
         /* After adding the new room, check all of worldData for floors to replace with outside corners */
-        // for (let i=0; i<mapHeight; i++) {
-        //   const y = i;
-        //   /* Every tile in that row */
-        //   for (let j=0; j<mapWidth; j++) {
-        //     const x = j;
+        for (let i=0; i<mapHeight; i++) {
+          const y = i;
+          /* Every tile in that row */
+          for (let j=0; j<mapWidth; j++) {
+            const x = j;
+        
+            let tileToN = false;
+            let tileToS = false;
+            let tileToE = false;
+            let tileToW = false;
+            if (y > 0) {
+              tileToN = worldData[y - 1][x];  
+            }
+            if (worldData[y + 1]) {
+              tileToS = worldData[y + 1][x]
+            }
+            if (worldData[y][x + 1]) {
+              tileToE = worldData[y][x + 1];
+            }
+            if (worldData[y][x - 1]) {
+              tileToW = worldData[y][x - 1];
+            }
 
+            if (worldData[y][x].type === 'ground' && 
+                tileToN.type === 'wall' && tileToN.wallType === 'wall_w' && 
+                tileToS.type === 'wall' && tileToS.wallType === 'wall_w'
+                ) {
+              console.log('we need to fill in a gap in a west wall')
+              worldData[y][x].backgroundImage = dungeonSprite;
+              worldData[y][x].type = 'wall';
+              worldData[y][x].facing = 'e';
+              worldData[y][x].wallType = 'wall_w';
+              worldData[y][x].spriteOffset = dungeonTiles.w;
+              worldData[y][x].z = 1;
+            }
 
-        //     const thisTile = worldData[y][x];
+            if (worldData[y][x].type === 'ground' && 
+                tileToN.type === 'wall' && tileToN.wallType === 'wall_e' && 
+                tileToS.type === 'wall' && tileToS.wallType === 'wall_e'
+                ) {
+              console.log('we need to fill in a gap in a east wall')
+              worldData[y][x].backgroundImage = dungeonSprite;
+              worldData[y][x].type = 'wall';
+              worldData[y][x].facing = 'w';
+              worldData[y][x].wallType = 'wall_e';
+              worldData[y][x].spriteOffset = dungeonTiles.e;
+              worldData[y][x].z = 1;
+            }
+
+            if (worldData[y][x].type === 'ground' && 
+                tileToN.type === 'wall' && tileToN.wallType === 'wall_n' && 
+                tileToS.type === 'wall' && tileToS.wallType === 'wall_n'
+                ) {
+              console.log('we need to fill in a gap in a north wall')
+              worldData[y][x].backgroundImage = dungeonSprite;
+              worldData[y][x].type = 'wall';
+              worldData[y][x].facing = 's';
+              worldData[y][x].wallType = 'wall_n';
+              worldData[y][x].spriteOffset = dungeonTiles.n;
+              worldData[y][x].z = 1;
+            }
             
-        //     // const existingTileType = worldData[y][x].type;
-        //     if (thisTile.type === 'ground') {
-        //       // console.log(tileToN);
-        //       if (tileToN.type === 'wall' && tileToN.facing === 'e' &&
-        //           tileToW.type === 'wall' && tileToW.facing === 's' &&
-        //           tileToS.type === 'ground' &&
-        //           tileToE.type === 'ground') {
-        //         console.log(`Outside southeast corner at X: ${x}, Y: ${y}!`);
-        //         worldData[y][x].type = 'wall';
-        //         worldData[y][x].facing = 'nw';
-        //         worldData[y][x].walkable = 'false';
-        //         worldData[y][x].backgroundImage = dungeonSprite;
-        //         worldData[y][x].spriteOffset = dungeonTiles.seo;
-        //       }
-        // //       } else if (tileToN && tileToN.type === 'wall' && tileToN.facing === 'west' &&
-        // //                   tileToE && tileToE.type === 'wall' && tileToE.facing === 'south' &&
-        // //                   tileToS && tileToS.type === 'ground' &&
-        // //                   tileToW && tileToW.type === 'ground') {
-        // //         console.log('Outside southwest corner!');
-        // //         worldData[y][x].type = 'wall';
-        // //         worldData[y][x].backgroundImage = dungeonSprite;
-        // //         worldData[y][x].spriteOffset = dungeonTiles.swo;
-        // //       } else if (tileToW && tileToW.type === 'wall' && 
-        // //                   tileToS && tileToS.type === 'wall' &&
-        // //                   tileToE && tileToE.type === 'ground' && 
-        // //                   tileToN && tileToN.type === 'ground') {
-        // //         console.log('Outside Northeast corner!');
-        // //         worldData[y][x].type = 'wall';
-        // //         worldData[y][x].backgroundImage = dungeonSprite;
-        // //         worldData[y][x].spriteOffset = dungeonTiles.neo;
-        // //       } else if (tileToE && tileToE.type === 'wall' &&
-        // //                   tileToS && tileToS.type === 'wall' &&
-        // //                   tileToW && tileToW.type === 'ground' &&
-        // //                   tileToN && tileToN.type === 'ground') {
-        // //         console.log('Outside Northwest corner!');
-        // //         worldData[y][x].type = 'wall';
-        // //         worldData[y][x].backgroundImage = dungeonSprite;
-        // //         worldData[y][x].spriteOffset = dungeonTiles.nwo;
-        // //       }
-        //     }
-        //   }
-        // }
+            if (worldData[y][x].type === 'ground' && 
+                tileToN.type === 'wall' && tileToN.wallType === 'wall_s' && 
+                tileToS.type === 'wall' && tileToS.wallType === 'wall_s'
+                ) {
+              console.log('we need to fill in a gap in a south wall')
+              worldData[y][x].backgroundImage = dungeonSprite;
+              worldData[y][x].type = 'wall';
+              worldData[y][x].facing = 'n';
+              worldData[y][x].wallType = 'wall_s';
+              worldData[y][x].spriteOffset = dungeonTiles.s;
+              worldData[y][x].z = 1;
+            }
+          }
+        }; // end fill-in check
       } else {
         console.log(`room doesn't fit, trying again`);
       }
