fd9393dcf61c03e9ccd30ae3a10d30097f07ec68
Max Lemieux <max.lemieux@gmail.com>
2020-05-22 10:00:09 -0700

    moved sprite info out of map component

--- src/components/map.js
+++ src/components/map.js
@@ -1,52 +1,12 @@
 import React from "react";
 import MapRow from "./mapRow";
+import spriteInfo from "../spriteInfo.js";
+
 const seedrandom = require('seedrandom');
 
 const RANDOM_KEY = 'fdsafdsfaoo';
 
-const dungeonSprite = '/img/environment/iso_dungeon_walls_by_pfunked.png';
-const dungeonTiles = {
-  n: [/* The same two plain ones repeated */
-        [32 + 128, 0],
-        [32 + 5 * 128, 0],
-        [32 + 128, 0],
-        [32 + 5 * 128, 0],
-        [32 + 128, 0],
-        [32 + 5 * 128, 0],
-        [32 + 128, 0],
-        [32 + 5 * 128, 0],
-        /* Fancy ones */
-        [32 + 5 * 128, 256],
-        [32 + 3 * 128, 256],
-        [32 + 1 * 128, 256],
-        [32 + 1 * 128, 384],
-        [32 + 7 * 128, 256]
-      ],
-  ne: [32 + 6 * 128, 128],
-  e: [32 + 2 * 128, 0],
-  se: [32 + 7 * 128, 128],
-  s: [32 + 3 * 128, 0],
-  sw: [32 + 4 * 128, 128],
-  w: [32, 0],
-  nw: [32 + 5 * 128, 128],
-  swo: [32 + 2 * 128, 128],
-  nwo: [32 + 3 * 128, 128],
-  neo: [32, 128],
-  seo: [32 + 1 * 128, 128],
-}
-
-const floorSprite = '/img/environment/tiles_0.png';
-const floorTiles = {
-  tiles: [[0, 0],
-          [64, 0],
-          [2 * 64, 0],
-          [3 * 64, 0],
-          [4 * 64, 0],
-          [0, 64],
-          [1 * 64, 0],
-          [2 * 64, 0],
-         ],
-};
+const { dungeonSprite, dungeonTiles, floorSprite, floorTiles } = spriteInfo;
 
 function Map(props) {
   const style = {
